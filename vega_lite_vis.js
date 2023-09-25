bb06c3c6fa087077e4b4473fc715af6a342b306a
monash-brandon-wee <126179133+monash-brandon-wee@users.noreply.github.com>
2023-09-25 16:48:12 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,15 @@
+var vg_1 = "vega_lite.json";
+
+vegaEmbed("#bar_chart", vg_1)
+  .then(function(result) {
+    // Access the Vega view instance as result.view
+    var view = result.view;
+    
+    // Now you can work with the Vega view
+    // For example, you can update data or interact with the visualization here
+    
+    // Example: Update data
+    var new_data = { /* your new data object */ };
+    view.change("source_0", vega.changeset().insert(new_data)).run();
+  })
+.catch(console.error);
